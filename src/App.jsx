253b36dfe8f97cdba06273f84908ce61bacaa311
João Vitor <joaovitor55670@gmail.com>
2024-05-21 15:30:55 -0300

    Configuração Inicial

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [actualIndex, setactualIndex] = useState(0)
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/data.js
@@ -0,0 +1,43 @@
+export const DATA_TEXT = [
+    {
+        title: 'Components',
+        description: 'Components are the building blocks of React applications. A component is a self-contained module (HTML + optional CSS + JS) that renders some output.',
+        code: `function Welcome() {
+    return <h1>Hello, World!</h1>;
+}`
+    },
+    {
+        title: 'Jsx',
+        description: 'JSX is a syntax extension to JavaScript. It is similar to a template language, but it has full power of JavaScript (e.g., it may output dynamic content).',
+        code: `<div>
+        <h1>Welcome {userName}</h1>
+        <p>Time to learn React!</p>
+</div>
+        `
+    },
+    {
+        title: 'Props',
+        description: 'Components accept arbitrary inputs called props. They are like function arguments.',
+        code: `function Welcome (props) {
+            return <h1>Hello, {props.name}</h1>;
+}`
+    },
+    {
+        title: 'State',
+        description: 'State allows React components to change their output over time in response to user actions, network responses, and anything else.',
+        code: `function Counter() {
+            const [isVisible, setIsVisible] = useState(false);
+          
+            function handleClick( )  { 
+                    setIsVisible(true);
+            }
+          
+            return (
+              <div>
+                    <button onClick={handleClick}>Show Details</button>
+                    {isVisible && <p>Amazing details!</p>}
+              </div>
+            )
+}`
+    }
+]
